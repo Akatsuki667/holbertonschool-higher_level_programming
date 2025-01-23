69274ed91b4ad000b4d2ee5cb646b5ce41600cd8
Akatsuki667 <qextzy@gmail.com>
2025-01-23 11:02:16 +0100

    pycodestyle

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,11 +2,11 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    
+
     max = my_list[0]
 
     for i in my_list:
         if i > max:
             max = i
-            
+
     return max
