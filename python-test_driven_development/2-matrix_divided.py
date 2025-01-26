b3526c51462a18ec30b47cf7b0398f515c15c424
Akatsuki667 <qextzy@gmail.com>
2025-01-26 16:12:06 +0100

    Updating README.md, adding 2-main.py, 2-matrix_divided.py, tests/2-matrix_divided.txt

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -33,9 +33,21 @@ def matrix_divided(matrix, div):
     [[0.18, 0.36, 0.55], [0.73, 0.91, 1.09]]
     """
 
-    if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) "
-                        "of integers/floats")
+    if not isinstance(matrix, list) or \
+       not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
+
+    for row in matrix:
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of "
+                                "integers/floats")
+
+    row_length = len(matrix[0])
+    for row in matrix:
+        if len(row) != row_length:
+            raise TypeError("Each row of the matrix must have the same size")
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
@@ -44,17 +56,9 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
 
     new_matrix = []
-
-    ref_row_len = 0
-
-    for i in matrix:
-
-        if not ref_row_len:
-            ref_row_len = len(i)
-
-        if len(i) != ref_row_len:
-            raise TypeError("Each row of the matrix must have the same size")
-
-        new_matrix.append(list(map(lambda x: round(x / div, 2), i)))
-
+    for row in matrix:
+        new_row = []
+        for element in row:
+            new_row.append(round(element / div, 2))
+        new_matrix.append(new_row)
     return new_matrix
