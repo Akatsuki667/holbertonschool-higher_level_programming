4110a0b8bf1ecf09a19bbd6758914443dfb87f45
Akatsuki667 <qextzy@gmail.com>
2025-01-28 13:49:48 +0100

    adding README.md, 0-main.py, 0-rectangle.py to Write an empty class Rectangle that defines a rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module `0-rectangle`
+
+Ce module définit une classe `Rectangle`, qui, pour l'instant ne fais rien.
+"""
+
+
+class Rectangle:
+    """
+    Représente un rectangle
+
+    Cette classe est vide, sert de base pour fonctionnalités futures.
+    """
+    pass
