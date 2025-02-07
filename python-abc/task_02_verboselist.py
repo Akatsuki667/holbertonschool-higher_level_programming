5b24569f42f80bda43de68dee38767ad691f02c7
Akatsuki667 <qextzy@gmail.com>
2025-02-07 12:50:24 +0100

    updating README.md, adding main_02_verboselist.py, task_02_verboselist.py

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -4,13 +4,15 @@
 class VerboseList(list):
 
     def append(self, item):
+        super().append(item)
         print(f"Added {[item]} to the list.")
 
     def extend(self, item):
         super().extend(item)
-        print(f"Extended the list with {[item]} items.")
+        print(f"Extended the list with {[len(item)]} items.")
 
     def remove(self, item):
+        super().remove(item)
         print(f"Removed {[item]} from the list.")
 
     def pop(self, index=-1):  # -1 par défaut pour suivre comportement list.pop
