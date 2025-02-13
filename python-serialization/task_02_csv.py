58773201eed0fc83894f106b565f86fd8aee9368
Akatsuki667 <qextzy@gmail.com>
2025-02-13 14:45:46 +0100

    updating README.md, adding main_02_csv.py, task_02_csv.py

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -4,10 +4,14 @@ import json
 
 
 def convert_csv_to_json(filename):
-    with open(filename, "r") as file:
-        reader = csv.DictReader(file)
-        data = list(reader)
-        print(data)
+    try:
+        with open(filename, "r") as file:
+            reader = csv.DictReader(file)
+            data = list(reader)
 
-    with open("data.json", "w") as f:
-        json.dump(data, f)
+        with open("data.json", "w") as f:
+            json.dump(data, f)
+        return True
+    except Exception as e:
+        print(f"{e}")
+        return False
