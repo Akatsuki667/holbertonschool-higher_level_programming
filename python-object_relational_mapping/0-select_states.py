33fb3f73ccb32a31f8a3d1663ce3ec773c401317
Akatsuki667 <qextzy@gmail.com>
2025-03-07 14:39:55 +0100

    adding 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,13 +3,14 @@
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3]
-)
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
 
 cur = db.cursor()
 
