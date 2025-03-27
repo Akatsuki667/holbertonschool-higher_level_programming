ffbfb59dbf5a458e25d4e386b3ab132030f6c63c
Akatsuki667 <qextzy@gmail.com>
2025-03-27 14:43:51 +0100

    Updating README.md, task_00_intro.py

---
+++ python-server_side_rendering/README.md
@@ -0,0 +1,4 @@
+# Python - Server-Side Rendering
+
+## 0. Creating a Simple Templating Program
+In this task, you will create a Python function that generates personalized invitation files from a template with placeholders and a list of objects. Each output file should be named sequentially, starting from 1. You will also implement specific error handling for various edge cases.
---
+++ python-server_side_rendering/task_00_intro.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def generate_invitations(template, attendees):
+    if type(template) is not str:
+        raise TypeError("Template must be a string")
+    if type(attendees) is not list:
+        raise TypeError("Attendees must be a list")
+    
+    if not template:
+        raise ValueError("Template is empty, no output files generated.")
+    if not attendees:
+        raise ValueError("No data provided, no output files generated.")
+    
+    for idx, attendee in enumerate(attendees, 1):
+        new_template = template
+
+        for key, value in attendee.items():
+            if value is None:
+                attendee[key] = "N/A"
+            
+            new_template = new_template.replace(f"{{{key}}}", str(value))
+
+        with open(f"output_{idx}.txt", "w") as file:
+            file.write(new_template)
+
+
+
